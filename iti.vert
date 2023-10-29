4b9a123f129207021a9a5fe7c657d88199d35b59
Eduardo Costa <m4c0@users.noreply.github.com>
2023-10-29 09:24:06 +0000

    playing around an instance buffer

--- iti.vert
+++ iti.vert
@@ -1,6 +1,10 @@
 #version 450
 
 layout(location = 0) in vec2 position;
+layout(location = 1) in vec2 xz0;
+layout(location = 2) in vec2 xz1;
+layout(location = 3) in float ceil_h;
+layout(location = 4) in float floor_h;
 
 layout(location = 0) out vec2 frag_coord;
 
--- poc.cpp
+++ poc.cpp
@@ -6,6 +6,48 @@ import casein;
 import vee;
 import voo;
 
+namespace {
+struct xz {
+  float x;
+  float z;
+};
+struct segment {
+  xz xz0;
+  xz xz1;
+  float ceil_h;
+  float floor_h;
+};
+class segbuf {
+  vee::buffer m_buf;
+  vee::device_memory m_mem;
+
+public:
+  static constexpr const auto max_segs = 1024;
+
+  explicit segbuf(vee::physical_device pd) {
+    m_buf = vee::create_vertex_buffer(sizeof(segment) * max_segs);
+    m_mem = vee::create_host_buffer_memory(pd, *m_buf);
+    vee::bind_buffer_memory(*m_buf, *m_mem, 0);
+
+    vee::mapmem mem{*m_mem};
+    auto *m = static_cast<segment *>(*mem);
+    m[0] = {};
+    m[0].xz0.x = -0.4;
+    m[0].xz1.x = -0.2;
+
+    m[1] = {};
+    m[1].xz0.x = 0.6;
+    m[1].xz1.x = 0.4;
+  }
+
+  void run(const voo::cmd_render_pass &scb, unsigned idx) const noexcept {
+    vee::cmd_bind_vertex_buffers(*scb, idx, *m_buf);
+  }
+
+  [[nodiscard]] constexpr auto size() const noexcept { return 2; }
+};
+} // namespace
+
 class thread : public voo::casein_thread {
 public:
   void run() override;
@@ -16,6 +58,7 @@ void thread::run() {
   auto cb = vee::allocate_primary_command_buffer(dq.command_pool());
 
   voo::one_quad quad{dq.physical_device()};
+  segbuf segs{dq.physical_device()};
 
   while (!interrupted()) {
     voo::swapchain_and_stuff sw{dq};
@@ -27,7 +70,17 @@ void thread::run() {
             voo::shader("iti.vert.spv").pipeline_vert_stage(),
             voo::shader("iti.frag.spv").pipeline_frag_stage(),
         },
-        {quad.vertex_input_bind()}, {quad.vertex_attribute(0)});
+        {
+            quad.vertex_input_bind(),
+            vee::vertex_input_bind_per_instance(sizeof(segment)),
+        },
+        {
+            quad.vertex_attribute(0),
+            vee::vertex_attribute_vec2(1, 0),
+            vee::vertex_attribute_vec2(1, sizeof(xz)),
+            vee::vertex_attribute_vec2(1, sizeof(xz) * 2),
+            vee::vertex_attribute_vec2(1, sizeof(xz) * 2 + sizeof(float)),
+        });
 
     resized() = false;
     while (!interrupted() && !resized()) {
@@ -38,7 +91,8 @@ void thread::run() {
 
         auto scb = sw.cmd_render_pass(cb);
         vee::cmd_bind_gr_pipeline(cb, *gp);
-        quad.run(scb, 0);
+        segs.run(scb, 1);
+        quad.run(scb, 0, segs.size());
       }
 
       sw.queue_submit(dq.queue(), cb);
